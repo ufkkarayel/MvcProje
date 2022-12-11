d316ecfb56b79835aafbfe193a6c0d900bd348a0
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2022-12-12 01:02:48 +0300

    12/12/2022

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+
+        // eklemek için genelde int kullanılır. 
+        int Insert(T p);
+        int Update(T p);
+        int Delete(T p);
+        T GetById(int id);
+    }
+}
